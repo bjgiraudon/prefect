3f3e012a98a7660d93aace124159336fadb48b37
bgiraudo <benjamin.giraudon@csgroup.eu>
2024-02-27 15:01:15 +0100

    feat: add flows

--- flows/redis_streams.py
+++ flows/redis_streams.py
@@ -1,32 +1,44 @@
 from prefect import flow, task
+
 # import geojson
 import redis
+from redis.typing import FieldT, EncodableT
 import os
 
-REDIS_STREAM_NAME = os.environ.get("REDIS_STREAM_NAME", "new:features")
+REDIS_STREAM_NAME = os.environ.get("REDIS_STREAM_NAME", "dedl:new_features")
+REDIS_CLIENT_PASSWORD = os.environ.get("REDIS_CLIENT_PASSWORD", None)
+
 
 @task(log_prints=True)
-def add_event(redis: redis.Redis, stream: str, payload: dict[redis.FieldT, redis.EncodableT]):
+def add_event(redis: redis.Redis, stream: str, payload: dict[FieldT, EncodableT]):
+    print(f"Redis XADD - stream_name:'{stream} with payload {payload}")
     redis.xadd(stream, payload)
 
-@flow(name="Redis Stream Events")
-def send_events(redis: redis.Redis, stream: str, events: list[dict[redis.FieldT, redis.EncodableT]]):
+
+@flow(name="Redis Stream Events", log_prints=True)
+def send_events(
+    redis: redis.Redis, stream: str, events: list[dict[FieldT, EncodableT]]
+):
     for event in events:
         add_event(redis, stream, event)
+
 
 if __name__ == "__main__":
     r = redis.Redis(
         host="localhost",
         port=6379,
-        password=os.environ.get("REDIS_CLIENT_PASSWORD"),
-        decode_responses=True)
+        password=REDIS_CLIENT_PASSWORD,
+        decode_responses=True,
+    )
 
-    events = [
-        {
-            "collection": "FOO",
-            "feature": "BAR",
-            "geometry": '{"type": "Point", "coordinates": [0, 0]}',
-        }
-    ]
-    
+    events = []
+    for i in range(10):
+        events.append(
+            {
+                "collection": "FOO",
+                "feature": f"BAR{i}",
+                "geometry": '{"type": "Point", "coordinates": [0, 0]}',
+            }
+        )
+
     send_events(r, REDIS_STREAM_NAME, events=events)
