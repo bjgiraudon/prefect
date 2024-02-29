64bba3e25e15b10d3dfd0c0dc3103ebcc9ea2f93
bgiraudo <benjamin.giraudon@csgroup.eu>
2024-02-29 15:37:58 +0100

    deploy: add redis streams deploy

--- flows/redis_streams.py
+++ flows/redis_streams.py
@@ -2,43 +2,53 @@ import os
 
 from prefect import flow, task
 import redis
-from redis.typing import FieldT, EncodableT
+from pydantic import SecretStr, BaseModel
 
 REDIS_CLIENT_HOST = os.environ.get("REDIS_CLIENT_HOST", "localhost")
 REDIS_CLIENT_PORT = int(os.environ.get("REDIS_CLIENT_PORT", "6379"))
-REDIS_CLIENT_PASSWORD = os.environ.get("REDIS_CLIENT_PASSWORD", None)
+REDIS_CLIENT_PASSWORD = SecretStr(os.environ.get("REDIS_CLIENT_PASSWORD", ""))
 REDIS_STREAM_NAME = os.environ.get("REDIS_STREAM_NAME", "dedl:new_features")
 
 
+class Event(BaseModel):
+    collection: str
+    feature: str
+    geometry: str
+
+
 @task(log_prints=True)
-def add_event(redis: redis.Redis, stream: str, payload: dict[FieldT, EncodableT]):
-    redis.xadd(stream, payload)
+def add_event(redis: redis.Redis, stream: str, payload: Event):
+    redis.xadd(stream, vars(payload))
 
 
 @flow(name="Redis Stream Events", log_prints=True)
 def send_events(
-    redis: redis.Redis, stream: str, events: list[dict[FieldT, EncodableT]]
-):
-    for event in events:
-        add_event(redis, stream, event)
-
-
-if __name__ == "__main__":
-    r = redis.Redis(
-        host=REDIS_CLIENT_HOST,
-        port=REDIS_CLIENT_PORT,
-        password=REDIS_CLIENT_PASSWORD,
+    host: str = REDIS_CLIENT_HOST,
+    port: int = REDIS_CLIENT_PORT,
+    password: SecretStr | None = REDIS_CLIENT_PASSWORD,
+    stream: str = REDIS_STREAM_NAME,
+    events: list[Event] = [],
+) -> None:
+    redis_client = redis.Redis(
+        host=host,
+        port=port,
+        password=password,
         decode_responses=True,
     )
 
+    for event in events:
+        add_event(redis_client, stream, event)
+
+
+if __name__ == "__main__":
     events = []
     for i in range(10):
         events.append(
-            {
-                "collection": "FOO",
-                "feature": f"BAR{i}",
-                "geometry": '{"type": "Point", "coordinates": [0, 0]}',
-            }
+            Event(
+                collection="FOO",
+                feature=f"BAR{i}",
+                geometry='{"type": "Point", "coordinates": [0, 0]}',
+            )
         )
 
-    send_events(r, REDIS_STREAM_NAME, events=events)
+    send_events(events=events)
